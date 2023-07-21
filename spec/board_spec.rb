9ce562511608367f6bb2cb842b1a1bac840a1d76
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-21 14:56:07 -0500

    Fix syntax errors in the spec for the valid_coordinate? method test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -18,3 +18,14 @@ RSpec.describe Board do
       expect(@board.cells).to eq({...})
     end
   end
+
+  describe "#valid_coordinate?" do
+    it "can determine if a coordinate is within the board's boundaries" do
+      expect(@board.valid_coordinate?("A1")).to be true
+      expect(@board.valid_coordinate?("D4")).to be true
+      expect(@board.valid_coordinate?("A5")).to be false
+      expect(@board.valid_coordinate?("E1")).to be false
+      expect(@board.valid_coordinate?("A22")).to be false
+    end
+  end
+end
