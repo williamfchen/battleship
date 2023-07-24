6ce0a0457e55c9c784f7a15f478505d986517a52
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-23 20:29:39 -0500

    Refactor the board class for the merge conflicts that came up

--- lib/board.rb
+++ lib/board.rb
@@ -7,24 +7,49 @@ class Board
 
   def create_cells
     cells = {}
-
     ("A".."D").each do |letter|
       (1..4).each do |num|
         coordinate = "#{letter}#{num}"
         cells[coordinate] = Cell.new(coordinate)
       end
-      
     end
     cells
   end
 
   def valid_coordinate?(coordinate)
-    cells.key?(coordinate)
+    cells.key?(coordinate) && cells[coordinate].empty?
   end
 
+  def valid_placement?(ship, coordinates)
+    return false unless ship.is_a?(Ship) && coordinates.is_a?(Array) && coordinates.length == ship.length
+    return false unless coordinates.all? do |coor|
+      valid_coordinate?(coor)
+    end
+    consecutive?(coordinates)
+  end
+
+  def consecutive?(coordinates)
+    chars_array = []
+    nums_array = []
+    coordinates.each do |coor|
+      chars_array << coor[0]
+      nums_array << coor[1]
+    end
+    if chars_array.uniq.count == 1
+      nums_array.each_cons(2).all? do |num1, num2|
+        num1.to_i + 1 == num2.to_i
+      end
+    elsif nums_array.uniq.count == 1
+      chars_array.each_cons(2).all? do |char1, char2|
+        char1.ord + 1 == char2.ord
+      end
+    else
+      false
+    end
+  end
 
   def place(ship, coordinates)
-      coordinates.each { |coordinate| cells[coordinate].place_ship(ship) } #if valid_placement?
+    coordinates.each { |coordinate| cells[coordinate].place_ship(ship) } if coordinates.all? { |coordinate| valid_coordinate?(coordinate) }
   end
 
   def render(reveal_ships = false)
@@ -39,40 +64,5 @@ class Board
       board_render += "\n"
     end
     puts board_render
-
-
-  def valid_placement?(ship, coordinates)
-    return false unless ship.is_a?(Ship) && coordinates.is_a?(Array) && coordinates.length == ship.length
-
-    return false unless coordinates.all? do |coor|
-      valid_coordinate?(coor)
-    end
-
-    consecutive?(coordinates)
-  end
-
-  def consecutive?(coordinates)
-    chars_array = []
-    nums_array = []
-
-    coordinates.each do |coor|
-      chars_array << coor[0]
-      nums_array << coor[1]
-    end
-
-    if chars_array.uniq.count == 1
-      nums_array.each_cons(2).all? do |num1, num2|
-        num1.to_i + 1 == num2.to_i
-      end
-
-    elsif nums_array.uniq.count == 1
-      chars_array.each_cons(2).all? do |char1, char2|
-        char1.ord + 1 == char2.ord
-      end
-
-    else
-      false
-    end
-
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -20,38 +20,12 @@ RSpec.describe Board do
   end
   
   describe "#valid_coordinate?" do
-    xit "can determine if a coordinate is within the board's boundaries" do
+    it "can determine if a coordinate is within the board's boundaries" do
       expect(@board.valid_coordinate?("A1")).to be true
       expect(@board.valid_coordinate?("D4")).to be true
       expect(@board.valid_coordinate?("A5")).to be false
       expect(@board.valid_coordinate?("E1")).to be false
       expect(@board.valid_coordinate?("A22")).to be false
-    end
-  end
-
-  describe '#places ships and renders' do
-    before(:each) do
-      @board.place(@cruiser, ["A1", "A2", "A3"])
-      @cell_1 = @board.cells["A1"]
-      @cell_2 = @board.cells["A2"]
-      @cell_3 = @board.cells["A3"]
-      @submarine = Ship.new("Submarine", 2) 
-    end
-
-    it '.place' do
-      expect(@cell_1.ship).to eq(@cruiser)
-      expect(@cell_2.ship).to eq(@cruiser)
-      expect(@cell_3.ship).to eq(@cruiser)
-      expect(@cell_3.ship == @cell_2.ship).to be true
-    end
-
-    it 'doesnt overlap' do
-      # add cell.empty? conditional to valid_placement
-      # expect(@board.valid_placement?(@submarine, ["A1", "B1"])).to be false
-    end
-
-    it '.renders' do
-      @board.render
     end
   end
 
@@ -78,4 +52,28 @@ RSpec.describe Board do
     end
   end
 
+  describe '#places ships and renders' do
+    before(:each) do
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+      @cell_1 = @board.cells["A1"]
+      @cell_2 = @board.cells["A2"]
+      @cell_3 = @board.cells["A3"]
+      @submarine = Ship.new("Submarine", 2) 
+    end
+
+    it '.place' do
+      expect(@cell_1.ship).to eq(@cruiser)
+      expect(@cell_2.ship).to eq(@cruiser)
+      expect(@cell_3.ship).to eq(@cruiser)
+      expect(@cell_3.ship == @cell_2.ship).to be true
+    end
+
+    it 'doesnt overlap' do
+      expect(@board.valid_placement?(@submarine, ["A1", "B1"])).to be false
+    end
+
+    it '.renders' do
+      @board.render
+    end
+  end
 end
